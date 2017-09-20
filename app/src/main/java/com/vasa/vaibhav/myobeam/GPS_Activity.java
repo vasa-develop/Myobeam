a757f90322bc6ecd03304639399044a2ba9bd886
vaibhav <vasa.develop@gmail.com>
2017-09-21 01:21:25 +0530

    Location-time app

---
+++ app/src/main/java/com/vasa/vaibhav/myobeam/GPS_Activity.java
@@ -0,0 +1,177 @@
+package com.vasa.vaibhav.myobeam;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class GPS_Activity extends AppCompatActivity {
+
+    private Button gps,save;
+    private TextView loc;
+    private Context context = this;
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    private String text="";
+
+    final static String fileName = "data.txt";
+    final static String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/instinctcoder/readwrite/" ;
+    final static String TAG = GPS_Activity.class.getName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gps_);
+
+        gps = (Button) findViewById(R.id.gps);
+        save = (Button) findViewById(R.id.save);
+        loc = (TextView) findViewById(R.id.location);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveToFile(text);
+                Toast.makeText(getApplicationContext(),"File saved as data.txt",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+                loc.setText(currentDateTimeString);
+
+
+                loc.append("\n" + "Latitude: " + location.getLatitude() + "\n" + "Longitude: " + location.getLongitude());
+
+                text = text+"Time: "+currentDateTimeString+" Latitude: " + location.getLatitude()+ "Longitude: " + location.getLongitude();
+                //loc.setText("\n" + "Latitude: " + location.getLatitude() + "\n" + "Longitude: " + location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+                String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+                loc.setText(currentDateTimeString);
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+                String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+                loc.setText(currentDateTimeString);
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.INTERNET
+                }, 10);
+            }
+        } else {
+            configureButton();
+        }
+
+        //locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    configureButton();
+                    return;
+                }
+        }
+    }
+
+    private void configureButton() {
+
+        gps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+                }
+            });
+
+    }
+
+    public static boolean saveToFile( String data){
+        try {
+            new File(path  ).mkdir();
+            File file = new File(path+ fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileOutputStream fileOutputStream = new FileOutputStream(file,true);
+            fileOutputStream.write((data + System.getProperty("line.separator")).getBytes());
+
+            return true;
+        }  catch(FileNotFoundException ex) {
+            Log.d(TAG, ex.getMessage());
+        }  catch(IOException ex) {
+            Log.d(TAG, ex.getMessage());
+        }
+        return  false;
+
+
+    }
+
+
+}
+
